f8eb76f268ce867f7545821e3a0c3b8d72de998c
David Kraft <kraft153@umn.edu>
2018-09-07 14:53:50 -0500

    Almost done with MVP for grading. Just need to make it work and hopefully incorporate auth.

--- client/src/App.js
+++ client/src/App.js
@@ -1,138 +1,21 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Login from "./pages/Login";
-// import Signup from "./pages/Signup"
-// import Home from "./pages/Home";
-// import Log from "./pages/Log"
-// import NewEntry from "./pages/NewEntry";
-// import EditEntry from "./components/EditEntry";
-
-// import Form from "./components/Form";
 import NavBar from "./components/NavBar";
-import PromptCard from "./components/PromptCard";
 import PostsPage from "./components/PostsPage";
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
 
   render() {
     return (
       <div>
         <NavBar />
-        <PromptCard />
         <PostsPage />
       </div>
     );
-  };
+  }
 }
 
 export default App;
-
-// // Use this stuff to make the forms work to save log entries
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {text: '', inputText: '', mode:'view'};
-//     class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {text: '', inputText: '', mode:'view'};
-
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSave = this.handleSave.bind(this);
-//     this.handleEdit = this.handleEdit.bind(this);
-//   }
-
-//   handleChange(e) {
-//     this.setState({ inputText: e.target.value });
-//   }
-
-//   handleSave() {
-//     this.setState({text: this.state.inputText, mode: 'view'});
-//   }
-
-//   handleEdit() {
-//     this.setState({mode: 'edit'});
-//   }
-
-//   render () {
-//     if(this.state.mode === 'view') {
-//       return (
-//         <div>
-//           <p>Text: {this.state.text}</p>
-//           <button onClick={this.handleEdit}>
-//             Edit
-//           </button>
-//         </div>
-//       );
-//     } else {
-//       return (
-//         <div>
-//           <p>Text: {this.state.text}</p>
-//             <input
-//               onChange={this.handleChange}
-//               value={this.state.inputText}
-//             />
-//           <button onClick={this.handleSave}>
-//             Save
-//           </button>
-//         </div>
-//       );
-//     }
-//   }
-// }
-
-// ReactDOM.render(
-//     <App />,
-//   document.getElementById('root')
-// );
-
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSave = this.handleSave.bind(this);
-//     this.handleEdit = this.handleEdit.bind(this);
-//   }
-
-//   handleChange(e) {
-//     this.setState({ inputText: e.target.value });
-//   }
-
-//   handleSave() {
-//     this.setState({text: this.state.inputText, mode: 'view'});
-//   }
-
-//   handleEdit() {
-//     this.setState({mode: 'edit'});
-//   }
-
-//   render () {
-//     if(this.state.mode === 'view') {
-//       return (
-//         <div>
-//           <p>Text: {this.state.text}</p>
-//           <button onClick={this.handleEdit}>
-//             Edit
-//           </button>
-//         </div>
-//       );
-//     } else {
-//       return (
-//         <div>
-//           <p>Text: {this.state.text}</p>
-//             <input
-//               onChange={this.handleChange}
-//               value={this.state.inputText}
-//             />
-//           <button onClick={this.handleSave}>
-//             Save
-//           </button>
-//         </div>
-//       );
-//     }
-//   }
-// }
-
-// ReactDOM.render(
-//     <App />,
-//   document.getElementById('root')
-// );
--- client/src/components/PostsPage.js
+++ client/src/components/PostsPage.js
@@ -1,112 +1,142 @@
-import React, { Component } from "react";
-import PromptCard from "./PromptCard"
-import { TextField } from "@material-ui/core";
+import React from "react";
+import TextField from '@material-ui/core/TextField';
 
-class PostsPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: [
-                {
-                    title: "Integer sit amet massa lobortis nulla rutrum.",
-                    entries:
-                        [
-                            {
-                                heading: 'Who was at the event?',
-                                body: "My uncle, Dick went with me. There were, I'd say, a few hundred people there.There were craft and food stands littered around the grounds.In the middle, there were stands and a circle with a bunch of chairs for tribal elder seating.The emcee started the opening ceremony by recognizing all of the veterans, then people danced behind the elders as they went around in a circle.",
-                                edit: false
-                            },
-                            {
-                                heading: "Nunc quis iaculis nisl. Quisque ut tellus eleifend.",
-                                body: "Sed a magna eu metus fermentum fringilla sit amet quis dui. Duis ultricies nibh eget tortor fringilla, sit amet lacinia ligula hendrerit. Etiam eros velit, fringilla et lacus sodales, ornare mollis augue. Pellentesque ornare velit massa, quis pharetra elit pharetra at. Maecenas condimentum, justo ac semper gravida, nunc nunc ullamcorper elit, id porta magna dui sodales lacus. Proin nec laoreet nisl. Sed nec quam fringilla, tempor justo vel, malesuada urna. Fusce commodo tincidunt posuere. Donec malesuada condimentum velit, ut mollis leo faucibus vel.",
-                                edit: false
-                            },
-                            {
-                                heading: "Nullam placerat consectetur vestibulum. Mauris tincidunt ligula nec.",
-                                body: "Vivamus laoreet, turpis eget viverra venenatis, diam nibh commodo est, iaculis dignissim turpis mi vel felis. Nam sapien ante, ornare nec egestas id, varius et felis. Nam sit amet lacus molestie, malesuada neque vitae, placerat urna. Cras et tortor arcu. Cras quis diam libero. Praesent condimentum risus eget urna gravida, a auctor dui pulvinar. Fusce eget suscipit sapien. Nullam luctus egestas dolor, ac facilisis ex ultrices eu. Mauris id mattis purus, condimentum facilisis sapien. In sit amet lacus vel justo porta lobortis nec quis ipsum.",
-                                edit: false
-                            },
-                            {
-                            }
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: auto,
+  },
+  menu: {
+    width: auto,
+  },
+});
 
-                        ],
-                    active: false
-                },
-                {
-                    title: "Lorem ipsum dolor sit amet.",
-                    entries:
-                        [
-                            {
-                                heading: 'Who was at the event?',
-                                body: "My uncle, Dick went with me. There were, I'd say, a few hundred people there.There were craft and food stands littered around the grounds.In the middle, there were stands and a circle with a bunch of chairs for tribal elder seating.The emcee started the opening ceremony by recognizing all of the veterans, then people danced behind the elders as they went around in a circle.",
-                                edit: false
-                            },
-                            {
-                                heading: "Nunc quis iaculis nisl. Quisque ut tellus eleifend.",
-                                body: "Sed a magna eu metus fermentum fringilla sit amet quis dui. Duis ultricies nibh eget tortor fringilla, sit amet lacinia ligula hendrerit. Etiam eros velit, fringilla et lacus sodales, ornare mollis augue. Pellentesque ornare velit massa, quis pharetra elit pharetra at. Maecenas condimentum, justo ac semper gravida, nunc nunc ullamcorper elit, id porta magna dui sodales lacus. Proin nec laoreet nisl. Sed nec quam fringilla, tempor justo vel, malesuada urna. Fusce commodo tincidunt posuere. Donec malesuada condimentum velit, ut mollis leo faucibus vel.",
-                                edit: false
-                            },
-                            {
-                                heading: "Nullam placerat consectetur vestibulum. Mauris tincidunt ligula nec.",
-                                body: "Vivamus laoreet, turpis eget viverra venenatis, diam nibh commodo est, iaculis dignissim turpis mi vel felis. Nam sapien ante, ornare nec egestas id, varius et felis. Nam sit amet lacus molestie, malesuada neque vitae, placerat urna. Cras et tortor arcu. Cras quis diam libero. Praesent condimentum risus eget urna gravida, a auctor dui pulvinar. Fusce eget suscipit sapien. Nullam luctus egestas dolor, ac facilisis ex ultrices eu. Mauris id mattis purus, condimentum facilisis sapien. In sit amet lacus vel justo porta lobortis nec quis ipsum.",
-                                edit: false
-                            },
-                        ],
-                    active: false
-                },
-                {
-                    title: "Integer porta massa nec urna tincidunt mollis. Fusce ultricies.",
-                    entries:
-                        [
-                            {
-                                heading: 'Who was at the event?',
-                                body: "My uncle, Dick went with me. There were, I'd say, a few hundred people there.There were craft and food stands littered around the grounds.In the middle, there were stands and a circle with a bunch of chairs for tribal elder seating.The emcee started the opening ceremony by recognizing all of the veterans, then people danced behind the elders as they went around in a circle.",
-                                edit: false
-                            },
-                            {
-                                heading: "Nunc quis iaculis nisl. Quisque ut tellus eleifend.",
-                                body: "Sed a magna eu metus fermentum fringilla sit amet quis dui. Duis ultricies nibh eget tortor fringilla, sit amet lacinia ligula hendrerit. Etiam eros velit, fringilla et lacus sodales, ornare mollis augue. Pellentesque ornare velit massa, quis pharetra elit pharetra at. Maecenas condimentum, justo ac semper gravida, nunc nunc ullamcorper elit, id porta magna dui sodales lacus. Proin nec laoreet nisl. Sed nec quam fringilla, tempor justo vel, malesuada urna. Fusce commodo tincidunt posuere. Donec malesuada condimentum velit, ut mollis leo faucibus vel.",
-                                edit: false
-                            },
-                            {
-                                heading: "Nullam placerat consectetur vestibulum. Mauris tincidunt ligula nec.",
-                                body: "Vivamus laoreet, turpis eget viverra venenatis, diam nibh commodo est, iaculis dignissim turpis mi vel felis. Nam sapien ante, ornare nec egestas id, varius et felis. Nam sit amet lacus molestie, malesuada neque vitae, placerat urna. Cras et tortor arcu. Cras quis diam libero. Praesent condimentum risus eget urna gravida, a auctor dui pulvinar. Fusce eget suscipit sapien. Nullam luctus egestas dolor, ac facilisis ex ultrices eu. Mauris id mattis purus, condimentum facilisis sapien. In sit amet lacus vel justo porta lobortis nec quis ipsum.",
-                                edit: false
-                            },
+export class PostsPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-                        ],
-                    active: false
-                }
-            ]
-        };
+    this.state = {
+      auth: false,
+      password: "pa55WOrd",
+      posts: [
+        {
+          title: "Post 1 of 3",
+          sections: [
+            {
+              heading: 'Section 1 of 3',
+              body: "My uncle, Dick went with me. There were, I'd say, a few hundred people there.There were craft and food stands littered around the grounds.In the middle, there were stands and a circle with a bunch of chairs for tribal elder seating.The emcee started the opening ceremony by recognizing all of the veterans, then people danced behind the elders as they went around in a circle."
+            },
+            {
+              heading: "Section 2 of 3",
+              body: "Sed a magna eu metus fermentum fringilla sit amet quis dui. Duis ultricies nibh eget tortor fringilla, sit amet lacinia ligula hendrerit. Etiam eros velit, fringilla et lacus sodales, ornare mollis augue. Pellentesque ornare velit massa, quis pharetra elit pharetra at. Maecenas condimentum, justo ac semper gravida, nunc nunc ullamcorper elit, id porta magna dui sodales lacus. Proin nec laoreet nisl. Sed nec quam fringilla, tempor justo vel, malesuada urna. Fusce commodo tincidunt posuere. Donec malesuada condimentum velit, ut mollis leo faucibus vel."
+            },
+            {
+              heading: "Section 3 of 3",
+              body: "Vivamus laoreet, turpis eget viverra venenatis, diam nibh commodo est, iaculis dignissim turpis mi vel felis. Nam sapien ante, ornare nec egestas id, varius et felis. Nam sit amet lacus molestie, malesuada neque vitae, placerat urna. Cras et tortor arcu. Cras quis diam libero. Praesent condimentum risus eget urna gravida, a auctor dui pulvinar. Fusce eget suscipit sapien. Nullam luctus egestas dolor, ac facilisis ex ultrices eu. Mauris id mattis purus, condimentum facilisis sapien. In sit amet lacus vel justo porta lobortis nec quis ipsum."
+            }
+          ]
+        },
+        {
+          title: "Post 2 of 3",
+          sections: [
+            {
+              heading: "Section 1 of 3",
+              body: "My uncle, Dick went with me. There were, I'd say, a few hundred people there.There were craft and food stands littered around the grounds.In the middle, there were stands and a circle with a bunch of chairs for tribal elder seating.The emcee started the opening ceremony by recognizing all of the veterans, then people danced behind the elders as they went around in a circle."
+            },
+            {
+              heading: "Section 2 of 3",
+              body: "Sed a magna eu metus fermentum fringilla sit amet quis dui. Duis ultricies nibh eget tortor fringilla, sit amet lacinia ligula hendrerit. Etiam eros velit, fringilla et lacus sodales, ornare mollis augue. Pellentesque ornare velit massa, quis pharetra elit pharetra at. Maecenas condimentum, justo ac semper gravida, nunc nunc ullamcorper elit, id porta magna dui sodales lacus. Proin nec laoreet nisl. Sed nec quam fringilla, tempor justo vel, malesuada urna. Fusce commodo tincidunt posuere. Donec malesuada condimentum velit, ut mollis leo faucibus vel."
+            },
+            {
+              heading: "Section 3 of 3",
+              body: "Vivamus laoreet, turpis eget viverra venenatis, diam nibh commodo est, iaculis dignissim turpis mi vel felis. Nam sapien ante, ornare nec egestas id, varius et felis. Nam sit amet lacus molestie, malesuada neque vitae, placerat urna. Cras et tortor arcu. Cras quis diam libero. Praesent condimentum risus eget urna gravida, a auctor dui pulvinar. Fusce eget suscipit sapien. Nullam luctus egestas dolor, ac facilisis ex ultrices eu. Mauris id mattis purus, condimentum facilisis sapien. In sit amet lacus vel justo porta lobortis nec quis ipsum."
+            }
+          ]
+        },
+        {
+          title: "Post 3 of 3.",
+          sections: [
+            {
+              heading: 'Section 1 of 3',
+              body: "My uncle, Dick went with me. There were, I'd say, a few hundred people there.There were craft and food stands littered around the grounds.In the middle, there were stands and a circle with a bunch of chairs for tribal elder seating.The emcee started the opening ceremony by recognizing all of the veterans, then people danced behind the elders as they went around in a circle.",
+            },
+            {
+              heading: "Section 2 of 3",
+              body: "Sed a magna eu metus fermentum fringilla sit amet quis dui. Duis ultricies nibh eget tortor fringilla, sit amet lacinia ligula hendrerit. Etiam eros velit, fringilla et lacus sodales, ornare mollis augue. Pellentesque ornare velit massa, quis pharetra elit pharetra at. Maecenas condimentum, justo ac semper gravida, nunc nunc ullamcorper elit, id porta magna dui sodales lacus. Proin nec laoreet nisl. Sed nec quam fringilla, tempor justo vel, malesuada urna. Fusce commodo tincidunt posuere. Donec malesuada condimentum velit, ut mollis leo faucibus vel.",
+            },
+            {
+              heading: "Section 3 of 3",
+              body: "Vivamus laoreet, turpis eget viverra venenatis, diam nibh commodo est, iaculis dignissim turpis mi vel felis. Nam sapien ante, ornare nec egestas id, varius et felis. Nam sit amet lacus molestie, malesuada neque vitae, placerat urna. Cras et tortor arcu. Cras quis diam libero. Praesent condimentum risus eget urna gravida, a auctor dui pulvinar. Fusce eget suscipit sapien. Nullam luctus egestas dolor, ac facilisis ex ultrices eu. Mauris id mattis purus, condimentum facilisis sapien. In sit amet lacus vel justo porta lobortis nec quis ipsum.",
+            },
+          ],
+        }
+      ]
     }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  };
 
-    // Define function to be used when someone opens a post.
-    toggleActive = (active) => active = !active;
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
 
-    render() {
-        return (
-            this.state.posts.forEach(element => {
+    this.setState({
+      [name]: value
+    });
+  }
 
-
-                // Checks to see if the journal entry is open. If not, it displays the list of entries.
-                if (element.active === false) {
-                    return (
-                        <ul>
-                            {this.state.posts.map((post) => (<li>{post.title}</li>))}
-                        </ul>
-
-                    )
-                } else {
-                    <PromptCard />
-                }
-
-            })
-        )
-
+  render() {
+    function toggleAuth() {
+      return (this.state.auth ? this.state.auth.setState = true : this.state.auth.setState = false);
     }
+    
+
+    listPosts = this.state.posts.map((post) => (
+      <div>
+        <h1>{post.title}</h1>
+        <form className={classes.container} noValidate autoComplete="off">
+          <TextField
+            name="Section 1"
+            label={post.sections[0].heading}
+            multiline
+            value={post.sections[0].body}
+            onChange={this.handleInputChange(value)}
+            className={classes.textField}
+            margin="normal"
+            InputProps={{
+              readOnly: true,
+            }}
+  
+          />
+          <TextField
+            name="Section 2"
+            label={post.sections[1].heading}
+            multiline
+            value={post.sections[1].body}
+            onChange={this.handleInputChange(value)}
+            className={classes.textField}
+            margin="normal"
+          />
+          <TextField
+            name="Section 3"
+            label={post.sections[2].heading}
+            multiline
+            value={post.sections[2].body}
+            onChange={this.handleInputChange(value)}
+            className={classes.textField}
+            margin="normal"
+          />
+        </form>
+      </div>
+    )
+    )
+  }
 }
-
 
 
 export default PostsPage;
